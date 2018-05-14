f7e415dbebd4b9263c83553f8ba162a89c01a323
Jason Slingerland <jslingerland@onshape.com>
2018-05-13 22:42:18 -0400

    finished tutorial

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # electronApps
+
+
+If you can build a website, you can build a desktop app. Electron is a framework for creating native applications with web technologies like JavaScript, HTML, and CSS. It takes care of the hard parts so you can focus on the core of your application.
+https://electronjs.org/
+
+----
+B.E.N.J.A: Bootable Electron Node JS Application
+It's NodeJS on top of Linux, delivered through an Electron instance that runs on the lightweight, Wayland driven, Weston compositor, falling back to a bare minimum accelerated Xorg.
+http://benja.io/
+
+## Shopping list application
+https://github.com/bradtraversy/electronshoppinglist
+https://www.youtube.com/watch?v=kN1Czs0m1SU
+
+### Installation
+
+Install the dependencies
+
+```sh
+$ npm install
+```
+
+### Serve
+To run electron
+
+```sh
+$ npm start
+```
+
+### To Package & Build
+
+For Windows
+
+```sh
+$ npm run package-win
+```
+
+For Mac
+
+```sh
+$ npm run package-mac
+```
+
+For Linux
+
+```sh
+$ npm run package-linux
+```
--- shoppingList/main.js
+++ shoppingList/main.js
@@ -2,17 +2,112 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, Menu, ipcMain} = electron;
+
+// SET ENV
+process.env.NODE_ENV = 'production';
 
 let mainWindow;
 
-// listen for app to be ready
+// Listen for app to be ready
 app.on('ready', function(){
   mainWindow = new BrowserWindow({});
-  //load html into window
+  // Load html into window
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'mainWindow.html'),
     protocol: 'file:',
     slashes: true
   }));
+
+  // Quit app when main window closed
+  mainWindow.on('closed', function(){
+    app.quit();
+  });
+
+  // Build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  // Insert menu
+  Menu.setApplicationMenu(mainMenu);
 });
+
+// Handle create add window
+function createAddWindow(){
+  // Create new window
+  addWindow = new BrowserWindow({
+    width:300,
+    height:200,
+    title:'Add shopping list item'
+  });
+  // Load html into window
+  addWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'addWindow.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+  // Garbage collection handle
+  addWindow.on('close', function(){
+    addWindow = null;
+  });
+};
+
+// Catch item:add
+ipcMain.on('item:add', function(e, item){
+  mainWindow.webContents.send('item:add', item);
+  addWindow.close();
+  // Still have a reference to addWindow in memory. Need to reclaim memory (Grabage collection)
+  //addWindow = null;
+});
+
+// Create menu template
+const mainMenuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Add item',
+        accelerator: process.platform == 'darwin' ? 'Command+N' : 'Ctrl+N',
+        click(){
+          createAddWindow();
+        }
+      },
+      {
+        label: 'Clear items',
+        accelerator: process.platform == 'darwin' ? 'Command+X' : 'Ctrl+X',
+        click(){
+          mainWindow.webContents.send('item:clear');
+        }
+      },
+      {
+        label: 'Quit',
+        accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click(){
+          app.quit();
+        }
+      }
+    ]
+  }
+];
+
+// If mac, add empty object to menu to show 'file'
+if (process.platform == 'darwin') {
+  mainMenuTemplate.unshift({});
+}
+
+// Add developer tools if not in prod
+if(process.env.NODE_ENV !== 'production'){
+  mainMenuTemplate.push({
+    label: "Developer tools",
+    submenu:[
+      {
+        label: 'Toggle DevTools',
+        accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+        click(item, focusedWindow){
+          focusedWindow.toggleDevTools();
+        }
+      },
+      {
+        role: 'reload'
+      }
+    ]
+  });
+}
